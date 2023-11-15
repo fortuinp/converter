1cee4e2522a7c33f6b03d4eabf92cff24f6c236d
fortuinp <pinda.fortuin@gmail.com>
2023-11-15 08:43:19 +0200

    update

--- script/code.js
+++ script/code.js
@@ -1,17 +1,37 @@
 let celciusValue = document.querySelector('[data-c]');
-let 
-let output=document.getElementById('output')
+
+let output= document.getElementById('output')
 
 
 function convertC(){
   // console.log(celciusValue.value);
   let fahrenheitValue = +celciusValue.value
-
- let result= (fahrenheitValue*9/5) + 32
+if (celciusValue.value) {
+  let result= (fahrenheitValue*9/5) + 32
+   
+  output.textContent= result + 'F';
   
- output.textContent= result + 'F';
+}else{
+  alert('type in value')
+}
 }
 document.getElementById('fahrenheitBtn').addEventListener('click', convertC)
 
 
 
+
+let fvalue=document.querySelector('[data-f]');
+
+function convertF(){
+  let celciusV = +fvalue.value
+
+  if (fvalue.value){
+  let result = (celciusV-32) * 5/9 
+  output.textContent= result + 'C';
+}
+else
+alert('type in value')
+
+}
+
+document.getElementById('celciusBtn').addEventListener('click', convertF)
